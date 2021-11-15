7e705ea3b399e7c8c1d7744382405cacd8a0c805
Pedro Henrique Costa <henriquepcnews@gmail.com>
2021-11-15 20:05:12 -0300

    - Adds Activity Screen

---
+++ FitAppIOS/Controller/ActivityViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityViewController.swift
+//  FitAppIOS
+//
+//  Created by Pedro Henrique on 15/11/21.
+//
+
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
